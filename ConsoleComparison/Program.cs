1ff9e8eebc5682f4ae9f92759380153cf172a6e0
Athul Raj <athulbraj@gmail.com>
2015-07-15 06:35:27 +0530

    First Commit. Comaprison with varied size support.

---
+++ ConsoleComparison/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using XnaFan.ImageComparison;
+
+
+namespace ConsoleComparison
+{
+
+    /// <summary>
+    /// Console program which compares two iages and returns the difference in percentage as an errorlevel between zero and a hundred (both included).
+    /// </summary>
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return -1;
+            }
+            else
+            {
+                //get, display and return the difference
+                int difference1 = (int)(ImageTool.GetPercentageDifference(args[0], args[1])*100);
+                Console.WriteLine("Difference is {0}%", difference1);
+                return difference1;
+            }
+        }
+    }
+}
